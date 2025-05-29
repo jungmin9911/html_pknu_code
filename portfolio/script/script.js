9c8c15eee40c2ae24d7ae7eabe94a45396dbc213
jungmin9911 <jmahn1819@gmail.com>
2025-05-29 17:32:46 +0900

    0529

---
+++ portfolio/script/script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+    new TypeIt('#typeit', {
+        speed: 90,
+        startDelay: 300,
+    })
+        .type('안녕하세요! 반갑습니다 :)', {delay: 1000})
+    //   .move(-6, {delay: 100})
+    //   .delete(15, {delay: 500})
+    //   .type("사실 안 반갑습니다. 집에 가고싶", {delay: 1000})
+        .go();
+});
