a0b30cb39b2593f97c146e3144849f8bff854dce
JungMin <jmahn1819@gmail.com>
2025-06-04 03:02:46 +0900

    포트폴리오

--- portfolio/script/script.js
+++ portfolio/script/script.js
@@ -9,3 +9,14 @@ document.addEventListener('DOMContentLoaded', function() {
     //   .type("사실 안 반갑습니다. 집에 가고싶", {delay: 1000})
         .go();
 });
+// document.addEventListener('DOMContentLoaded', function() {
+//     new TypeIt('#typeit2', {
+//         speed: 90,
+//         startDelay: 300,
+//     })
+//         .type('신입 백엔드 개발자', {delay: 1000})
+//     //   .move(-6, {delay: 100})
+//     //   .delete(15, {delay: 500})
+//     //   .type("사실 안 반갑습니다. 집에 가고싶", {delay: 1000})
+//         .go();
+// });s
